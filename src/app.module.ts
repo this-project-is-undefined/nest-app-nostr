60cc792c3b98fc0014ef1096c8a27230c0185d19
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-02 19:26:24 -0300

    Creating user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModulexw],
   controllers: [AppController],
   providers: [AppService],
 })
